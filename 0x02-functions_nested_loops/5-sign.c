fd46ce11e72832914836866dfba05f0a467d7262
DevBventurez <bonveturew@gmail.com>
2023-10-12 13:48:07 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,8 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(23);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
-	{
-		_putchar(55);
-		return 0;
-	}
-	else
-	{
-		_putchar(-5);
-		return -1;
-	}
 }
