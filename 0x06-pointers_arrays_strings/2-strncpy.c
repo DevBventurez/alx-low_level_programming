514fb519730b02511936b04f3af5e9625aa7f321
DevBventurez <bonveturew@gmail.com>
2023-10-20 02:16:48 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
-     	
-	j = 0;
+	int j = 0;
+
 	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
-
 	}
 	while (j < n)
 	{
