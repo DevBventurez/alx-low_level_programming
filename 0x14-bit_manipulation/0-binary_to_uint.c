92f1456e2ec87b1514d48d75b613df2e82feebf6
DevBventurez <bonveturew@gmail.com>
2023-11-28 06:26:03 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,25 @@
-nclude "main.h"
+include "main.h
+
 /**
- * 
- * binary_to_ubit - a program that converts binary numbers
- * to integers/decimal numbers
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * @b: a character pointer to a string containing
- * binary numbers
- *
- * Return: the converted decimal (output)
+ * Return: converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, i, j, sum, pow;
-	int base;
+	int i;
+	unsigned int dec_val = 0;
 
-	base = 2;
-	sum = 0;
-	pow = 1;
-
-	if (b == NULL)
+	if (!b)
 		return (0);
-	for (len = 0; b[len] I= '\0'; len++)
-		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1')
-			return (b[0] - 48);
-	for (i = 0 b[i] != '\0'; i++)
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
-		return (0);
-		for (j = len - 1; j > 0; j--)
-		pow = pow * base;
-		sum = sum + (pow * (b[i] - 48));
-		len--;
-		pow = 1;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (sum);
+
+	return (dec_val);
 }
